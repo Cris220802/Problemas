068477e8ec8fc29c22f49828c06385290927aafa
Cris220802 <72528616+Cris220802@users.noreply.github.com>
2020-10-22 18:50:26 -0500

    Add files via upload

---
+++ Problema_1_Calificaciones.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	int a, b, c, p;
+	
+	cin>>a;
+	cin>>b;
+	cin>>c;
+	
+	p = (a + b + c) / 3;
+	
+	
+	p >= 6 ? cout<<1 : cout<<0;
+	
+
+	
+	
+	
+}
